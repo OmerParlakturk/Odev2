b532866161aae707a86babacf793d0fea4b83cc2
OmerParlakturk <head2story@gmail.com>
2019-03-06 19:30:21 +0300

    latest form of the code

--- src/test/java/TestCompute.java
+++ src/test/java/TestCompute.java
@@ -15,8 +15,52 @@ public class TestCompute {
     assertTrue(true);
   }
 
+  @Test
+  public void exampleTest1() {
+    MessageQueue mq = mock(MessageQueue.class);
+    c = new Compute(mq);
 
+    when(c.mq.size()).thenReturn(0);
+
+    assertEquals(-1 , c.countNumberOfOccurrences("Random"));
+  }
+
+  @Test
+  public void exampleTest2() {
+    MessageQueue mq = mock(MessageQueue.class);
+    c = new Compute(mq);
+     
+    when(c.mq.size()).thenReturn(5);
+
+    assertEquals(0 , c.countNumberOfOccurrences("Random"));
+  }
+  public void exampleTest3(){
+    MessageQueue mq = mock(MessageQueue.class);
+    c = new Compute(mq);
+    
+    when(c.mq.size()).thenReturn(2);
+    
+    when(c.mq.contains("Random")).thenReturn(false);
+
+    assertEquals(0 , c.countNumberOfOccurrences("Random"));
+
+  }
+
+  @Test
+  public void exampleTest4(){
+   MessageQueue mq = mock(MessageQueue.class);
+   c = new Compute(mq);
  
-  
+    when(c.mq.size()).thenReturn(4);
+    when(c.mq.contains("Random")).thenReturn(true);
+    when(c.mq.getAt(0)).thenReturn("Random");
+    when(c.mq.getAt(1)).thenReturn("Unknown");
+    when(c.mq.getAt(2)).thenReturn("Unknown2");
+    when(c.mq.getAt(3)).thenReturn("Random");
+    assertEquals(2 , c.countNumberOfOccurrences("Random"));
+
+
+  }
+
   
 }
